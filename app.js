6ca6a236e59e0caa1d10ea5ba8528db2b05e01b4
Mustafo <mustafomurtazaev7@gmail.com>
2026-09-13 13:09:01 +0500

    cors for local react development

--- app.js
+++ app.js
@@ -3,10 +3,14 @@ const app = express();
 const db = require("./config/db");
 const PORT = 1111;
 const router = require("./routes/router");
+const cors = require("cors");
 require("dotenv").config();
 
 db();
 
+app.use(cors({
+  origin: "http://localhost:5173"
+}));
 app.use(express.json());
 app.use("/", router);
 
